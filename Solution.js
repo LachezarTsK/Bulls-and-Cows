e23ae87161ec21a23042bdddf6b80b4cb9ef6f29
Lachezar Kutsarov <lkutsarov@gmail.com>
2025-07-07 13:03:56 +0200

    Create Solution.js

---
+++ Solution.js
@@ -0,0 +1,30 @@
+
+/**
+ * @param {string} secret
+ * @param {string} guess
+ * @return {string}
+ */
+var getHint = function (secret, guess) {
+    const NUMBER_OF_DIGITS = 10;
+    const ASCII_ZERO = 48;
+    let numberOfBulls = 0;
+    let numberOfCows = 0;
+
+    const nonBullFrequencyInSecret = new Array(NUMBER_OF_DIGITS).fill(0);
+    const nonBullFrequencyInGuess = new Array(NUMBER_OF_DIGITS).fill(0);
+
+    for (let i = 0; i < secret.length; ++i) {
+        if (secret.charAt(i) === guess.charAt(i)) {
+            ++numberOfBulls;
+            continue;
+        }
+        ++nonBullFrequencyInSecret[secret.codePointAt(i) - ASCII_ZERO];
+        ++nonBullFrequencyInGuess[guess.codePointAt(i) - ASCII_ZERO];
+    }
+
+    for (let digit = 0; digit < NUMBER_OF_DIGITS; ++digit) {
+        numberOfCows += Math.min(nonBullFrequencyInSecret[digit], nonBullFrequencyInGuess[digit]);
+    }
+
+    return numberOfBulls + "A" + numberOfCows + "B";
+};
